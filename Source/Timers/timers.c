5a4e98543d75de9ac4daaa06e1a8e575629c44dc
Andrés Tosca <atr.andres@gmail.com>
2014-09-24 11:34:10 -0500

    Adding some important functions for the library

--- Source/Timers/timers.c
+++ Source/Timers/timers.c
@@ -1,5 +1,8 @@
-#import "timers.h"
-
+#include "timers.h"
+void (*func[10])(void);
+int tim[10];
+int contadores[10];
+int pos = 0;
 
 void timer3Config(void){
 	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; //TIM3 clock enable
@@ -9,4 +12,28 @@ void timer3Config(void){
 	
 	TIM3->DIER |= 1<<0; //Enable interrupt
 	NVIC->ISER[0] |= 1 << 29;	//Enable TIM3 interrupt in NVIC
+}
+
+
+void setTimer(void (*function)(void), int timer)
+	{
+		func[pos] = function;
+		tim [pos] = timer;
+		pos ++;	
+	}
+
+	
+void TIM3_IRQHandler (void) 
+	{
+	
+	if(TIM3->SR & TIM_SR_UIF) {
+		TIM3->SR &= ~TIM_SR_UIF; 
+		contadores[0] ++;
+		if(contadores[0] == tim[0])
+		{
+			func[0]();
+			contadores[0] = 0;
+		}
+			
+}
 }
--- Source/Timers/timers.h
+++ Source/Timers/timers.h
@@ -1,3 +1,10 @@
 #include "stm32f30x.h"
+extern void (*func[10])(void);
+extern int tim[10];
+extern int contadores[10]; 
+
+
 
 void timer3Config(void);
+void setTimer(void (*function)(void), int timer);
+void TIM3_IRQHandler (void);
