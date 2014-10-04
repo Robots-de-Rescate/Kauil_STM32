824f6d68b1492483f0326a2e731613cea0e543a3
Andrés Tosca <atr.andres@gmail.com>
2014-10-04 12:14:45 -0500

    Fixing timers library

--- Source/Timers/timers.c
+++ Source/Timers/timers.c
@@ -4,6 +4,7 @@ void (*func[10])(void);
 int tim[10];
 int contadores[10];
 int pos = 0;
+int c;
 
 void timer3Config(void){
 	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; //TIM3 clock enable
@@ -29,11 +30,13 @@ void TIM3_IRQHandler (void)
 	if(TIM3->SR & TIM_SR_UIF) 
 		{
 		TIM3->SR &= ~TIM_SR_UIF; 
-		contadores[0] ++;
-		if(contadores[0] == tim[0])
-			{
-			func[0]();
-			contadores[0] = 0;
+			for(c = 0; c <= sizeof(contadores)/sizeof(contadores[0]); c++){
+				contadores[c] ++;
+				if(contadores[c] == tim[c])
+					{
+					func[c]();
+					contadores[c] = 0;
+				}
 			}
 		}
 	}
